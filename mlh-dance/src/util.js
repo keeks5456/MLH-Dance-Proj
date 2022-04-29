f5958438894181b1cb993c5ddd62c2c233f73286
Franco <phanfranco@yahoo.com>
2022-04-29 15:00:54 -0700

    feat(instructors): instructors page setup, utility to generate teachers build.

---
+++ mlh-dance/src/util.js
@@ -0,0 +1,47 @@
+import { faker } from "@faker-js/faker";
+
+export const getInstructors = (number) => {
+  let styles = [
+    "hip-hop",
+    "salsa",
+    "ballet",
+    "house",
+    "hip-hop",
+    "salsa",
+    "ballet",
+    "house",
+    "hip-hop",
+    "salsa",
+    "ballet",
+    "house",
+  ];
+  const instructors = [];
+  let i = 1;
+  while (i <= number) {
+    const name = faker.name.findName();
+    const email = faker.internet.email();
+    const phone = faker.phone.phoneNumber();
+    const image = faker.image.people(400, 600);
+    let date = faker.date.between(
+      "2015-01-01T00:00:00.000Z",
+      "2022-01-01T00:00:00.000Z"
+    );
+    date = date.getMonth() + "/" + date.getDay() + "/" + date.getFullYear();
+
+    instructors.push({
+      id: i,
+      name: name,
+      email: email,
+      phone: phone,
+      image: image,
+      date: date,
+    });
+    i++;
+  }
+
+  for (let i = 0; i < number; i++) {
+    instructors[i].style = styles[i];
+  }
+  console.log(instructors);
+  return instructors;
+};
