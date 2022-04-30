c9095e09d5b83faeb173cb497941722b3aba325a
Akiko Green <agreen@alphaworks.tech>
2022-04-30 10:43:14 -0700

    style(About Pg)

--- mlh-dance/src/Pages/About/About.js
+++ mlh-dance/src/Pages/About/About.js
@@ -1,18 +1,36 @@
-import React from 'react'
-import './About.css'
+import React from "react";
+import "./About.css";
 const About = () => {
   return (
-    <div className='about-container'>
-    <div className='header-contianer'>
-      <h1 className='about-header'>
-      <i>Like No One's Watching</i>About
-      </h1>
+    <div className="about-container">
+      <div className="header-contianer">
+        <h1 className="about-header">
+          <i>Like No One's Watching</i>About
+        </h1>
+      </div>
+      <div className="image-container">
+        <img
+          className="about-image-1"
+          src="https://le-cdn.hibuwebsites.com/f5666827ac6141668bc3e640a0e4d609/dms3rep/multi/opt/Hero2-1920w.jpg"
+          alt="dancing woman"
+        ></img>
+      </div>
+      <div className="discription-container">
+        <p className="about-discription">
+          Ever wanted to join a dance class, but were too self-conscious about
+          your skill level compared to everyone else? <br />
+          Maybe you're an advanced to intermediate dancer looking for private
+          training but cring ate the prices mos studies have to offer? Well, look no further!<br/>
+          <br/>
+          <em>Like No One's Watching</em> is a web application that connects users with all levels of dance skills to dance teachers all over the world!
+          <br/>
+          Whether you're <b>Novice, Beginner, Advance or Intermediate</b>, we have a plethora of dance instructors for all sorts of dance background for your choosing.
+        </p>
+      </div>
+  
     </div>
-    <div className='image-container'>
-      <image className='about-image' src="https://le-cdn.hibuwebsites.com/f5666827ac6141668bc3e640a0e4d609/dms3rep/multi/opt/Hero2-1920w.jpg" alt="dancing woman"></image>
-    </div>
-    </div>
-  )
-}
+  );
+};
 
-export default About
+export default About;
+//&nbsp;
--- mlh-dance/src/Pages/Home/Home.js
+++ mlh-dance/src/Pages/Home/Home.js
@@ -7,7 +7,7 @@ const Home = () => {
   return (
     <div className="home-container">
       <div className="home-title-container">
-        <h1 id="h1-title">Like No-One's Watching</h1>
+        <h1 id="h1-title">Like No One's Watching</h1>
       </div>
       <div className="dance-image-container">
         <img
